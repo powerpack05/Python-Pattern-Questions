6f0e972b0c83fbea8676186842b85ce112d4fe9e
Syed <102013740+powerpack05@users.noreply.github.com>
2024-04-14 17:48:30 +0530

    hallow_pyramid

---
+++ hallow_pyramid.py
@@ -0,0 +1,17 @@
+
+
+def hallow_pyramid(number_of_rows):
+
+    for i in range(1, number_of_rows + 1):
+
+        for j in range(1, 2 * number_of_rows):
+
+            if (i + j == number_of_rows + 1) or (j - i == number_of_rows - 1) or (i == j and j%2!=0):
+
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+hallow_pyramid(9)
+
